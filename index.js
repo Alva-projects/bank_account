a24011d01c0ff58647ec5afd52fcef45a4ef5bcb
Alva-projects <alva.lundberg@hotmail.se>
2025-11-04 23:00:31 +0100

    empty methods added: withdrawal, getAccountName,getBalance

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const account = {
     accountName: "Spongebob",
     balance: 100,
-    getBalance: function(){
-                console.log(account.balance)
-    }
+    getBalance: function() {
+        console.log(this.balance)
+    },
     deposit: function() {
         this.balance= (this.balance + 10)
+    },
+    withdrawal: function() {
+
+    },
+    getAccountName: function() {
+
     }
 }
 account.getBalance();
