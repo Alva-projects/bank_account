3fb975b941be7575ddb6679f5a079296b1a19842
Alva-projects <alva.lundberg@hotmail.se>
2025-11-04 23:46:20 +0100

    adding accountError

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const account = {
-    accountName: "Spongebob",
-    balance: 100,
+    accountName: "Spongebob", /* This is the bank account holder's name */
+    balance: 100, /* This is your starting balance */
     getBalance: function() {
         console.log(this.balance)
     },
     deposit: function() {
-        this.balance = this.balance + 10;
+        this.balance = this.balance + 10; /* <-- Make a deposit by changing this number */
     },
     withdrawal: function() {
-        this.balance - 50
+        this.balance = this.balance - 50 /* <-- Make a withdrawal by changing this number */
     },
     getAccountName: function() {
-        console.log(this.accountName)
+        console.log(this.accountName);
     },
+    accountError: function() {
+        let enterName = "Spongebo";
+        if (this.accountName !== enterName){
+            console.log("You really shouldn't steal other people's money...")
+        }
+    }
 }
 console.log(account.accountName);
+account.withdrawal();
 account.deposit();
 account.getBalance();
 
