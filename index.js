c932b7c744149e614d5ac1d4c12172aa770a2765
Alva Lundberg <alva.lundberg@hotmail.se>
2025-11-20 11:07:02 +0100

    atm structure

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const account = {
     getBalance: function() {
         console.log("Current balance: " + this.balance)
     },
-}
+};
 
 if (account.accountError() === true) {
    account.deposit();
@@ -36,4 +36,20 @@ if (account.accountError() === true) {
     account.getBalance(); 
 } else {
     console.log("You really shouldn't steal other people's money...");
-}
+}; 
+
+function atm() {
+    const message = parseFloat (
+        prompt (
+            "Select a choice 1.) See balance 2.) Make a deposit 3.) Make a withdrawal 4.) Get account name 5.) Exit"
+        )
+    );
+
+    switch (message) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+    }
+};
